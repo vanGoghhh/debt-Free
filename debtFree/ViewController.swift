8941d41b93f3197b742ca7455de97999fa9d0713
Nigel <blackznigelng@gmail.com>
2020-06-01 11:04:59 +0800

    Initial Commit

---
+++ debtFree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  debtFree
+//
+//  Created by Nigel Ng on 1/6/20.
+//  Copyright © 2020 Nigel Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
